ac3db0b32f2583640779107447fb4d3f50e4a9c5
Xalkan <xalkan@pm.me>
2026-02-16 19:17:15 -0300

    add support for submarine swaps

--- src/error.rs
+++ src/error.rs
@@ -141,6 +141,9 @@ pub enum APIError {
     #[error("Invalid fee rate: {0}")]
     InvalidFeeRate(String),
 
+    #[error("Invalid HTLC params: {0}")]
+    InvalidHtlcParams(String),
+
     #[error("Invalid indexer: {0}")]
     InvalidIndexer(String),
 
@@ -366,7 +369,6 @@ impl From<RgbLibError> for APIError {
             }
             RgbLibError::InvalidAddress { details } => APIError::InvalidAddress(details),
             RgbLibError::InvalidAmountZero => APIError::InvalidAmount(s!("0")),
-            RgbLibError::InvalidAssetID { asset_id } => APIError::InvalidAssetID(asset_id),
             RgbLibError::InvalidAssignment => APIError::InvalidAssignment,
             RgbLibError::InvalidAttachments { details } => APIError::InvalidAttachments(details),
             RgbLibError::InvalidDetails { details } => APIError::InvalidDetails(details),
@@ -454,6 +456,7 @@ impl IntoResponse for APIError {
             | APIError::InvalidDetails(_)
             | APIError::InvalidEstimationBlocks
             | APIError::InvalidFeeRate(_)
+            | APIError::InvalidHtlcParams(_)
             | APIError::InvalidInvoice(_)
             | APIError::InvalidMediaDigest
             | APIError::InvalidName(_)
