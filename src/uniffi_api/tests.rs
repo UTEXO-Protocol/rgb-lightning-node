833ab4ae8b2fc66a9889d2e88e38b44938173b76
dcorral <hi@dcorral.com>
2026-04-15 14:05:05 +0200

    Fix UniFFI smoke tests

--- src/uniffi_api/tests.rs
+++ src/uniffi_api/tests.rs
@@ -97,6 +97,8 @@ mod uniffi_smoke_tests {
                 database: Arc::new(database),
                 lsp_base_url: None,
                 lsp_bearer_token: None,
+                vss_url: None,
+                vss_unencrypted: false,
             }),
             cancel_token: CancellationToken::new(),
             unlocked_app_state: Arc::new(TokioMutex::new(None)),
